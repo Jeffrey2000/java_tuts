e12cdf19312b64c72e1523d65b247f9c7217acb2
Jeffrey2000 <jeffltd@outlook.com>
2020-02-01 00:47:49 +0000

    Gaining basic understanding of method overloading

--- mainProgram.java
+++ mainProgram.java
@@ -1,6 +1,25 @@
 public class mainProgram {
     public static void main(String[] args){
-        SpeedConverter speedConverter = new SpeedConverter();
-        speedConverter.printConversion(1.5);
+        //Gaining Basic understanding of method overloading
+        System.out.println(calcFeetAndInchesToCentimeters(6,2));
     };
+
+    public static double calcFeetAndInchesToCentimeters(int feet, int inches){
+        if(feet >=0 && (inches >=0 && inches <=12)){
+            inches+= feet * 12;
+            double centimeters = inches * 2.54;
+            return centimeters;
+        }
+        return -1;
+    }
+
+
+    public static double calcFeetAndInchesToCentimeters(int inches){
+        if (inches >= 0){
+            int remInches = inches % 12;
+            int feet = (inches  - remInches) / 12;
+            return calcFeetAndInchesToCentimeters(feet, remInches);
+        }
+        return -1;
+    }
 }
