872cee5e8bce5075b9703439c5ff52872815062c
Jeffrey2000 <jeffltd@outlook.com>
2020-07-06 16:51:06 +0100

    Answers to tutorials for loop challenges

--- Tutorial.java
+++ Tutorial.java
@@ -6,9 +6,44 @@ public class Tutorial {
 	
 	public static void main(String[] args) {
 		for (int i=2; i <=8; ++i) {
-			System.out.println("10000 at an interest rate of " + i + " " +  calculateInterest(10000,i));
+			System.out.println("10000 at an interest rate of " + i + "% " +  calculateInterest(10000,i));
 		}
+		
+		System.out.println("Going backwards");
+		
+		for (int i=8; i >=2; --i) {
+			System.out.println("10000 at an interest rate of " + i + "% " +  calculateInterest(10000,i));
+		}
+		
+		int count = 0;
+		
+		for(int i=20; i >=1; --i) {
+			if(count==3) {
+				break;
+			}else {
+				if(isPrime(i)) {
+					count+=1;
+					System.out.println(i + " is a prime number");
+				}
+			}
+		}
+		
 	}
+	
+	public static boolean isPrime(int number) {
+		if (number == 1) {
+			return false;
+		}else {
+			for(int i=2; i < number/2; i++) {
+				if(number % i == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	
 	
 
 }
